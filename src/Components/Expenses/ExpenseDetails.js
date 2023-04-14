174a870778448e97e9399ca472ca2e98cb878659
preranasinghh <preranasingh1176@gmail.com>
2023-04-14 12:40:06 +0530

    first

---
+++ src/Components/Expenses/ExpenseDetails.js
@@ -0,0 +1,5 @@
+function ExpenseDetails(props) {
+    return <div className="expense-item__price">${props.amount}</div>;
+  }
+  
+  export default ExpenseDetails;
---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
+import ExpenseChart from './ExpenseChart';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
